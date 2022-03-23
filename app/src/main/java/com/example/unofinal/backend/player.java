199686186306bcd3a7fcc3c2a29cd58c6582f96a
Med1z1n <99459061+Med1z1n@users.noreply.github.com>
2022-03-23 14:21:02 -0700

    Added Player class

---
+++ app/src/main/java/com/example/unofinal/backend/player.java
@@ -0,0 +1,43 @@
+package com.example.unofinal.backend;
+
+import java.util.*;
+
+public class player {
+	private ArrayList<MainCard> hand = new ArrayList<>();
+	
+	public player(Stack<MainCard> drawPile) {
+		for (int i = 0; i < 7; i++) {
+			hand.add(drawPile.pop());
+		}
+	}
+	
+	
+	public int handSize() {
+		return hand.size();
+	}
+	
+	public void DrawCards(int num, Stack<MainCard> drawPile) {
+		for (int i = 0; i < num; i++) {
+			hand.add(drawPile.pop());
+		}
+	}
+	
+	public boolean canMove(MainCard.Color color, MainCard mostRecent) {
+		for (MainCard c : hand) {
+    		if (c.matches(mostRecent) || c.getColor() == color) {
+    			return true;
+    		}
+    	}
+    	return false;
+	}
+	
+	public void playCard(MainCard card, Stack<MainCard> discard) { // plays the card i.e. discard
+    	for (int i = 0; i < hand.size(); i++) {
+    		if (hand.get(i) == card) {
+    			hand.remove(i);
+    		}
+    	}
+    	discard.push(card);
+    }
+
+}
